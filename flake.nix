f489d83133c28ead2f6ac9b8a6ae619262beb6e8
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2026-02-06 23:14:59 +0100

    added macbook

--- flake.nix
+++ flake.nix
@@ -1,13 +1,16 @@
 {
+  description = "NixOS configuration with flakes";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
   outputs =
     {
       self,
       nixpkgs,
       disko,
+      nixos-hardware,
       ...
     }@inputs:
     {
@@ -20,8 +23,20 @@
         };
         modules = [
           disko.nixosModules.disko
-          ./configuration.nix
-          ./hardware-configuration.nix
+          ./servers/vps2.de-berlin.net.inckmann.de/configuration.nix
+          ./servers/vps2.de-berlin.net.inckmann.de/hardware-configuration.nix
+        ];
+      };
+
+      nixosConfigurations.mbp-2016 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          nixos-hardware.nixosModules.apple-macbook-pro-14-1
+          ./mbp-2016/configuration.nix
+          ./mbp-2016/hardware-configuration.nix
         ];
       };
     };
