5318afe6e68a162e0753f3c949e2653dcb10ca3f
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2026-02-07 00:00:57 +0100

    added touchbar

--- mbp-2016/configuration.nix
+++ mbp-2016/configuration.nix
@@ -25,6 +25,8 @@
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  hardware.apple.touchbar.enable = true;
+
   networking.hostName = "mbp-2016"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
