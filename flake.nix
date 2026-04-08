a809af85b8fe9e9130bf9980566f7136000e8b5e
Maximilian Inckmann <maximilian@inckmann.de>
2026-04-08 18:33:14 +0200

    Added VPS2 Berlin again

--- flake.nix
+++ flake.nix
@@ -1,13 +1,33 @@
 {
 inputs = {
-nixpkgs.url = "github:NixOs/nixpkgs/nixos-25.11";
-sops-nix = {
-	url = "github:Mic92/sops-nix";
-	inputs.nixpkgs.follows = "nixpkgs";
-};
+	nixpkgs.url = "github:NixOs/nixpkgs/nixos-25.11";
+	sops-nix = {
+		url = "github:Mic92/sops-nix";
+		inputs.nixpkgs.follows = "nixpkgs";
+	};
+	disko = {
+		url = "github:nix-community/disko";
+		inputs.nixpkgs.follows = "nixpkgs";
+	};
 };
 outputs = {self, nixpkgs, ...}@inputs: {
-	nixosConfigurations.t420 = nixpkgs.lib.nixosSystem rec {
+	nixosConfigurations = {
+          vps2-de-berlin = nixpkgs.lib.nixosSystem rec {
+		pkgs = import nixpkgs {
+			inherit system;
+			config.allowUnfree = true;
+		};
+		system = "x86_64-linux";
+		specialArgs = {
+			inherit inputs self;
+		};
+		modules = [
+		  inputs.disko.nixosModules.disko
+		  ./servers/vps2.de-berlin.net.inckmann.de/configuration.nix
+		  ./servers/vps2.de-berlin.net.inckmann.de/hardware-configuration.nix
+		]; 
+	  };
+	  t420 = nixpkgs.lib.nixosSystem rec {
 		pkgs = import nixpkgs {
 				inherit system;
 				config.allowUnfree = true;
@@ -21,6 +41,7 @@ outputs = {self, nixpkgs, ...}@inputs: {
     			./nix-config.nix
 			inputs.sops-nix.nixosModules.sops
 		];
+	};
 	};
 };
 }
