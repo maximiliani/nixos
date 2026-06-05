46b0e5e71f5714cf99ed107bd48ba7247065d4ea
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2026-06-06 00:45:57 +0200

    Add macbook pro 2016 configuration to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,48 +1,63 @@
 {
 inputs = {
-	nixpkgs.url = "github:NixOs/nixpkgs/nixos-25.11";
-	sops-nix = {
-		url = "github:Mic92/sops-nix";
-		inputs.nixpkgs.follows = "nixpkgs";
-	};
-	disko = {
-		url = "github:nix-community/disko";
-		inputs.nixpkgs.follows = "nixpkgs";
-	};
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-25.11";
+    sops-nix = {
+        url = "github:Mic92/sops-nix";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+        url = "github:nix-community/disko";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
 };
 outputs = {self, nixpkgs, ...}@inputs: {
-	nixosConfigurations = {
-          vps2-de-berlin = nixpkgs.lib.nixosSystem rec {
-		pkgs = import nixpkgs {
-			inherit system;
-			config.allowUnfree = true;
-		};
-		system = "x86_64-linux";
-		specialArgs = {
-			inherit inputs self;
-		};
-		modules = [
-		  inputs.sops-nix.nixosModules.sops
-		  inputs.disko.nixosModules.disko
-		  ./servers/vps2.de-berlin.net.inckmann.de/configuration.nix
-		  ./servers/vps2.de-berlin.net.inckmann.de/hardware-configuration.nix
-		]; 
-	  };
-	  t420 = nixpkgs.lib.nixosSystem rec {
-		pkgs = import nixpkgs {
-				inherit system;
-				config.allowUnfree = true;
-			};
-		system = "x86_64-linux";
-		specialArgs = {
-			inherit inputs self;
-		};
-		modules = [
-			./configuration.nix
-    			./nix-config.nix
-			inputs.sops-nix.nixosModules.sops
-		];
-	};
-	};
+   nixosConfigurations = {
+      vps2-de-berlin = nixpkgs.lib.nixosSystem rec {
+         pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+         };
+         system = "x86_64-linux";
+         specialArgs = {
+            inherit inputs self;
+         };
+         modules = [
+           inputs.sops-nix.nixosModules.sops
+           inputs.disko.nixosModules.disko
+           ./servers/vps2.de-berlin.net.inckmann.de/configuration.nix
+           ./servers/vps2.de-berlin.net.inckmann.de/hardware-configuration.nix
+         ];
+      };
+      t420 = nixpkgs.lib.nixosSystem rec {
+        pkgs = import nixpkgs {
+                inherit system;
+                config.allowUnfree = true;
+            };
+        system = "x86_64-linux";
+        specialArgs = {
+            inherit inputs self;
+        };
+        modules = [
+            ./configuration.nix
+                ./nix-config.nix
+            inputs.sops-nix.nixosModules.sops
+        ];
+      };
+      mbp-2016 = nixpkgs.lib.nixosSystem rec {
+        pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+        };
+        system = "x86_64-linux";
+        specialArgs = {
+            inherit inputs self;
+        };
+        modules = [
+            ./mbp-2016/configuration.nix
+            ./mbp-2016/hardware-configuration.nix
+            inputs.sops-nix.nixosModules.sops
+        ];
+      };
+    };
 };
 }
